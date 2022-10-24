d85b960d53fa3f88e60964b5969498723ae069cb
al <k4t4kun@gmail.com>
2022-10-24 15:59:37 +0600

    add Makefile Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,8 @@
 package hexlet.code;
 
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Cli.helloUser();
     }
 }
