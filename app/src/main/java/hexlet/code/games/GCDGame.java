76b75be36ccaf2f7e1e9071cfdf11f838193d243
al <k4t4kun@gmail.com>
2022-10-27 11:47:05 +0600

    fix GCDGame.java

--- app/src/main/java/hexlet/code/games/GCDGame.java
+++ app/src/main/java/hexlet/code/games/GCDGame.java
@@ -37,55 +37,17 @@ public class GCDGame {
     }
 
     private static int GCD(int num1, int num2) {
-        int[] dividers1 = simpleDivs(num1);
-        int[] dividers2 = simpleDivs(num2);
-        int[] sameDiv = new int[Math.max(dividers1.length, dividers2.length)];
-        int count = 0;
-        int gdc = 1;
-
-        for (int i = 0; i < dividers1.length; i++) {
-            for (int j = 0; j < dividers2.length; j++) {
-                if (dividers1[i] == dividers2[j]) {
-                    sameDiv[i] = dividers1[i];
-                    count++;
-                }
+        int gcd = 1;
+        for (int i = 1; i <= num1 && i <= num2; i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                gcd = i;
             }
         }
-        sameDiv = Arrays.copyOf(sameDiv, count);
-        int[] uniqSameDivs = removeDuplicates(sameDiv);
-        for (int div : uniqSameDivs) {
-            gdc *= div;
-        }
-        return gdc;
+        return gcd;
     }
 
-    private static int[] removeDuplicates (int[] arr) {
-        int count = 0;
-        int[] uniqArr = new int[arr.length];
-        // arr is sorted
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] != arr[i + 1]) {
-                uniqArr[count++] = arr[i];
-                count++;
-            }
-        }
-        return Arrays.copyOf(uniqArr, count);
-    }
 
-    private static int[] simpleDivs(int num) {
-        int divider = 2;
-        int[] dividers = new int[100];
-        int count = 0;
-        while (num > 1) {
-            if (num % divider == 0) {
-                dividers[count] = divider;
-                num = num / divider;
-                count++;
-            } else {
-                divider++;
-            }
-        }
-        return Arrays.copyOf(dividers, count);
-    }
+
+
 
 }
