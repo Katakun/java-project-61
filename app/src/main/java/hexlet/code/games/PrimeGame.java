c2d0e721fa2ea2832984185bc899cc4f028acb0e
al <k4t4kun@gmail.com>
2022-10-28 09:49:37 +0600

    fix PrimeGame.java

--- app/src/main/java/hexlet/code/games/PrimeGame.java
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -32,6 +32,9 @@ public class PrimeGame {
     }
 
     private static boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
         for (int i = 2; i < number / 2; i++) {
             if (number % i == 0) {
                 return false;
